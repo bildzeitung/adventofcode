42753b8e5c66781833faef7a04c4d1cae0d91023
daniel.klein <daniel.klein@oracle.com>
2015-12-08 09:48:07 -0500

    Thought of a minor optimisation

--- 8/part1.sh
+++ 8/part1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# change \\ to .
-# change \x00 to ---
-# change \" to + 
-# remove all letters
+# change \[\|"] to +
+# change \x to ---
+# remove all letters & numbers
 #
 # This leaves us with the delta between coding and actual
 # so just get the length and then sum them all up
-perl -ne 'chomp ; s/\\\\/./g ; s/\\x[0-9a-f][0-9a-f]/---/g ; s/\\"/+/g ; s/[a-z]//g ; print length($_), "\n"' < matchsticks.in | paste -sd+ - | bc
+perl -ne 'chomp; s/\\[^x]/+/g; s/\\x/---/g; s/\w//g ; print length($_), "\n"' < matchsticks.in | paste -sd+ - | bc
