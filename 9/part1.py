31ed632a0c328385730c3ebcd3a95a053e54a549
daniel.klein <daniel.klein@oracle.com>
2015-12-09 01:03:32 -0500

    Day 9

---
+++ 9/part1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# The key property here is that this is a complete graph; that makes the
+# algorithm a brute force O(n^2) affair
+#
+import sys
+
+from collections import defaultdict
+from copy import deepcopy
+
+# load in data
+master = defaultdict(dict)
+with open(sys.argv[1]) as source:
+    for line in source:
+        a, _, b, _, dist = line.rstrip().split(' ')
+        master[a][b] = int(dist)
+        master[b][a] = int(dist)
+
+# simple greedy search
+def doit(start, master):
+    total = 0 
+    while master[start]:
+        dest = master[start]
+        shortest = sorted(dest, key=dest.get)[0]
+        print '%s -> %s (%s)' % (start, shortest, dest[shortest]) 
+        total += dest[shortest]
+        del master[start]
+        for value in master.itervalues():
+            del value[start]
+        start = shortest
+
+    print '---> %s' % total
+    return total
+
+print sorted([doit(start, deepcopy(master)) for start in master.keys()])[0]
