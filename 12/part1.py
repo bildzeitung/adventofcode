f75c2d1bcfeab99ace6140213f5bfb8678ce0e40
daniel.klein <daniel.klein@oracle.com>
2015-12-12 00:19:02 -0500

    Part 1

---
+++ 12/part1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" Day 12 """
+
+import json
+import sys
+
+SOURCE = json.load(open(sys.argv[1]))
+
+def do_total(data):
+    """ Sum all numbers """
+    total = 0
+    if hasattr(data, 'itervalues'):
+        gen = data.itervalues()
+    else:
+        gen = data
+
+    for item in gen:
+        if isinstance(item, list):
+            #total += do_list_total(v)
+            total += do_total(item)
+        elif isinstance(item, dict):
+            total += do_total(item)
+        elif isinstance(item, basestring):
+            continue
+        elif isinstance(item, int):
+            total += item
+        else:
+            raise ValueError('Help: %s' % item)
+
+    return total
+
+print 'FINAL: %s' % do_total(SOURCE)
