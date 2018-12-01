288fe0a384da7f350dcd05a7b1cc6f4e8a45135b
bildzeitung <bildeitung@gmail.com>
2018-12-01 09:19:25 -0500

    Due to earlier bug, missed that I just read the vals in; fixed

--- 2018/01/p1.py
+++ 2018/01/p1.py
@@ -3,18 +3,8 @@ from pathlib import Path
 
 
 def main():
-    total = 0
-    p = Path('./input.txt')
-    with p.open() as f:
-        for line in f:
-            r = line.strip()
-            if r.startswith('+'):
-                r = int(r[1:])
-            else:
-                r = int(r)
-            total += r
-
-    print(f"TOTAL: {total}")
+    with Path('./input.txt').open() as f:
+        print(f"TOTAL: {sum(int(x.strip()) for x in f)}")
 
 
 if __name__ == '__main__':
