29276269320833e7718deea973b10dd04efce249
bildzeitung <bildeitung@gmail.com>
2018-12-01 09:22:30 -0500

    Thanks, Reddit for suggestion of using "cycle"; again, I learn something about the python stdlib

--- 2018/01/p2.py
+++ 2018/01/p2.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 from pathlib import Path
+from itertools import cycle
 
 seen = {0}  # set, primed with 0
 
 
-def get_val(line):
-    r = line.strip()
-    if r.startswith('+'):
-        return int(r[1:])
-    return int(r)
-
-
 def main():
-    p = Path('./input.txt')
-
     ''' Read the program (all the integers)
     '''
     items = []
-    with p.open() as f:
-        items = [get_val(l) for l in f]
+    with Path('./input.txt').open() as f:
+        items = [int(l) for l in f]
 
     ''' Loop assuming the data will cause it to halt.
 
@@ -26,13 +18,12 @@ def main():
         seen. Run through the array repeatedly, as per the problem description
     '''
     total = 0
-    while (True):
-        for item in items:
-            total += item
-            if total in seen:
-                print(f"TOTAL: {total}")
-                return
-            seen.add(total)
+    for item in cycle(items):
+        total += item
+        if total in seen:
+            print(f"TOTAL: {total}")
+            return
+        seen.add(total)
 
 
 if __name__ == '__main__':
