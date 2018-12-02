2001ab339b560c81857cf947b8ec8c51d2ca7d6e
bildzeitung <bildeitung@gmail.com>
2018-12-02 13:31:14 -0500

    Tighten up solution

--- 2018/02/p1.py
+++ 2018/02/p1.py
@@ -5,26 +5,14 @@ from collections import Counter
 from pathlib import Path
 
 
-def get_checksum(word):
-    has_two = 0
-    has_three = 0
-    c = dict(Counter(word))
-    if 2 in c.values():
-        has_two = 1
-    if 3 in c.values():
-        has_three = 1
-
-    return has_two, has_three
-
-
 def main():
     total_twos = 0
     total_threes = 0
     with Path(sys.argv[1]).open() as f:
         for word in f:
-            twos, threes = get_checksum(word.strip())
-            total_twos += twos
-            total_threes += threes
+            c = dict(Counter(word))
+            total_twos += 2 in c.values()
+            total_threes += 3 in c.values()
 
     print(f"2's: {total_twos}  3's: {total_threes} "
           f"==> {total_twos * total_threes}")
