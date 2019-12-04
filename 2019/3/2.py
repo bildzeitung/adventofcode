30e20c5c7085f8d4afad7306f92e4e2cfce77c8e
bildzeitung <bildzeitung@gmail.com>
2019-12-03 22:42:37 -0500

    More cleanup

--- 2019/3/2.py
+++ 2019/3/2.py
@@ -7,47 +7,32 @@ from collections import defaultdict
 from itertools import repeat
 from pathlib import Path
 
-dirs = {"R": (1, 0),
-        "L": (-1, 0),
-        "U": (0, 1),
-        "D": (0, -1)}
+dirs = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 
 def main():
     with Path(sys.argv[1]).open() as f:
-        path1 = [
-            repeat(dirs[x[0]], int(x[1:])) 
-            for x in f.readline().strip().split(",")
-            ]
-        path2 = [
-            repeat(dirs[x[0]], int(x[1:])) 
-            for x in f.readline().strip().split(",")
+
+        def readpath():
+            return [
+                repeat(dirs[x[0]], int(x[1:])) for x in f.readline().strip().split(",")
             ]
 
-    steps1 = defaultdict(lambda: sys.maxsize)
-    steps2 = defaultdict(lambda: sys.maxsize)
+        def calcsteps(points):
+            s = (0, 0)
+            idx = 0
+            steps = defaultdict(lambda: sys.maxsize)
+            for i in points:
+                for x in i:
+                    idx += 1
+                    s = (s[0] + x[0], s[1] + x[1])
+                    steps[s] = min(steps[s], idx)
+            return steps
 
-    s = (0, 0)
-    idx = 0
-    e1 = set()
-    for i in path1:
-        for x in i:
-            idx += 1
-            s = (s[0]+x[0], s[1]+x[1])
-            e1.add(s)
-            steps1[s] = min(steps1[s], idx)
+        steps1 = calcsteps(readpath())
+        steps2 = calcsteps(readpath())
 
-    s = (0, 0)
-    idx = 0
-    e2 = set()
-    for i in path2:
-        for x in i:
-            idx += 1
-            s = (s[0]+x[0], s[1]+x[1])
-            e2.add(s)
-            steps2[s] = min(steps2[s], idx)
-
-    print(min([steps1[i] + steps2[i] for i in e1 & e2]))
+    print(min([steps1[i] + steps2[i] for i in steps1.keys() & steps2.keys()]))
 
 
 if __name__ == "__main__":
