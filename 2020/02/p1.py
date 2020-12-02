ee08bc0b2878f8c1d8871ef4f949eb7ddccf8aae
bildzeitung <bildzeitung@gmail.com>
2020-12-02 10:00:07 -0500

    Cleanup, comments

--- 2020/02/p1.py
+++ 2020/02/p1.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 """
   Advent 2
+
+  Evaluate a string given a rule, all data-driven
+
 """
 import sys
-from collections import Counter
 
 
 def is_valid(line):
+  """ Evaluate password against the rule
+
+      1-2 x: xpxc
+      | | |  ^^^^- password
+      | |  +- char to count
+      | +---- upper end of range
+      +------ low end of range
+
+      n.b. ranges are INCLUSIVE
+  """
   rulepart, password = [x.strip() for x in line.split(":")]
-  p = Counter(password)
   rangepart, item = rulepart.split(' ')
   low, high = [int(x) for x in rangepart.split('-')]
-  return low <= p[item] <= high
+  #
+  # TIL! Strings have built-in counters!
+  # >>> "foo".count("o") == 2
+  #
+  return low <= password.count(item) <= high
 
 
 def main():
--- 2020/02/p2.py
+++ 2020/02/p2.py
@@ -3,13 +3,27 @@
   Advent 2
 """
 import sys
-from collections import Counter
 
 
 def is_valid(line):
+  """ Evaluate password against the rule
+
+      1-2 x: xpxc
+      | | |  ^^^^- password
+      | |  +- char to check
+      | +---- position 1
+      +------ position 2
+
+      n.b. positions are 1-indexed, so adjust
+
+      Rule is that at least one, but not both positions contain the
+      character to check.
+  """
   rulepart, password = [x.strip() for x in line.split(":")]
   rangepart, item = rulepart.split(' ')
   p1, p2 = [int(x) for x in rangepart.split('-')]
+  #
+  # TIL! XOR bitwise operator works properly against bool()
   return (password[p1-1] == item) ^ (password[p2-1] == item)
 
 
