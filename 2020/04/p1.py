b989cda141a129a8b60129413599d3c8c6cc0fbf
bildzeitung <bildzeitung@gmail.com>
2020-12-04 00:57:31 -0500

    Tighten up

--- 2020/04/p1.py
+++ 2020/04/p1.py
@@ -4,15 +4,18 @@
 """
 import sys
 
-FIELDS = set([  "byr",  # (Birth Year)
-            "iyr",  # (Issue Year)
-            "eyr",  # (Expiration Year)
-            "hgt",  # (Height)
-            "hcl",  # (Hair Color)
-            "ecl",  # (Eye Color)
-            "pid",  # (Passport ID)
-#            "cid",  # (Country ID)
-])
+FIELDS = set(
+    [
+        "byr",  # (Birth Year)
+        "iyr",  # (Issue Year)
+        "eyr",  # (Expiration Year)
+        "hgt",  # (Height)
+        "hcl",  # (Hair Color)
+        "ecl",  # (Eye Color)
+        "pid",  # (Passport ID)
+        #            "cid",  # (Country ID)
+    ]
+)
 
 
 def read_passport(f):
@@ -21,26 +24,26 @@ def read_passport(f):
         l = l.strip()
         if not l:
             return passport
-        passport.update(dict(entry.split(':') for entry in l.split(' ')))
+        passport.update(dict(entry.split(":") for entry in l.split(" ")))
     return passport
 
 
 def is_valid(passport):
-    k = [x for x in passport.keys()]
+    if "cid" in passport:
+        del passport["cid"]
 
-    if 'cid' in k:
-        k.remove("cid")
-
-    return len(set(k) ^ FIELDS) == 0
+    return len(set(passport.keys()) ^ FIELDS) == 0
 
 
 def main():
     with open(sys.argv[1]) as f:
+
         def feed():
             while passport := read_passport(f):
                 yield is_valid(passport)
 
         return sum(x for x in feed())
+
 
 if __name__ == "__main__":
     print(main())
--- 2020/04/p2.py
+++ 2020/04/p2.py
@@ -5,20 +5,23 @@
 import re
 import sys
 
-FIELDS = set([  "byr",  # (Birth Year)
-            "iyr",  # (Issue Year)
-            "eyr",  # (Expiration Year)
-            "hgt",  # (Height)
-            "hcl",  # (Hair Color)
-            "ecl",  # (Eye Color)
-            "pid",  # (Passport ID)
-#            "cid",  # (Country ID)
-])
+FIELDS = set(
+    [
+        "byr",  # (Birth Year)
+        "iyr",  # (Issue Year)
+        "eyr",  # (Expiration Year)
+        "hgt",  # (Height)
+        "hcl",  # (Hair Color)
+        "ecl",  # (Eye Color)
+        "pid",  # (Passport ID)
+        #  "cid",  # (Country ID)
+    ]
+)
 
 EYES = ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
-hair = re.compile('^#[0-9a-f]{6}$')
-id = re.compile('^[0-9]{9}$')
+hair = re.compile("^#[0-9a-f]{6}$")
+id = re.compile("^[0-9]{9}$")
 
 
 def read_passport(f):
@@ -27,56 +30,42 @@ def read_passport(f):
         l = l.strip()
         if not l:
             return passport
-        passport.update(dict(entry.split(':') for entry in l.split(' ')))
+        passport.update(dict(entry.split(":") for entry in l.split(" ")))
     return passport
 
 
 def is_valid(passport):
-    k = [x for x in passport.keys()]
+    if "cid" in passport:
+        del passport["cid"]
 
-    if 'cid' in k:
-        k.remove("cid")
-
-    if len(set(k) ^ FIELDS) != 0:
+    # completeness check; don't do anything unless this passes
+    if len(set(passport.keys()) ^ FIELDS) != 0:
         return False
 
-    if not 1920 <= int(passport['byr']) <= 2002:
-        return False
-
-    if not 2010 <= int(passport['iyr']) <= 2020:
-        return False
-
-    if not 2020 <= int(passport['eyr']) <= 2030:
-        return False
-
-    if 'cm' in passport['hgt']:
-        if not 150 <= int(passport['hgt'][:-2]) <= 193:
-            return False
-    elif 'in' in passport['hgt']:
-        if not 59 <= int(passport['hgt'][:-2]) <= 76:
-            return False
-    else:
-        return False
-
-    if not hair.search(passport['hcl']):
-        return False
-
-    if passport['ecl'] not in EYES:
-        return False
-
-    if not id.search(passport['pid']):
-        return False
-
-    return True
+    # all remaining criteria
+    return all(
+        [
+            1920 <= int(passport["byr"]) <= 2002,
+            2010 <= int(passport["iyr"]) <= 2020,
+            2020 <= int(passport["eyr"]) <= 2030,
+            ("cm" in passport["hgt"] and 150 <= int(passport["hgt"][:-2]) <= 193)
+            or ("in" in passport["hgt"] and 59 <= int(passport["hgt"][:-2]) <= 76),
+            hair.search(passport["hcl"]),
+            passport["ecl"] in EYES,
+            id.search(passport["pid"]),
+        ]
+    )
 
 
 def main():
     with open(sys.argv[1]) as f:
+
         def feed():
             while passport := read_passport(f):
                 yield is_valid(passport)
 
         return sum(x for x in feed())
+
 
 if __name__ == "__main__":
     print(main())
