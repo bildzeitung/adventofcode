a3c3c1624d7926b8e9e64ed211fbd3516b78c124
bildzeitung <bildzeitung@gmail.com>
2020-12-05 00:41:13 -0500

    Lolz

--- 2020/05/p1.py
+++ 2020/05/p1.py
@@ -5,22 +5,17 @@
 import sys
 
 
-def narrow(bsp, rb):
-    """ Kind of a brute-force binary partition
+def narrow(bsp):
+    """ Type conversion is hard; let Python do it
     """
-    left, right = 0, rb
-    for k in bsp:
-        if k == "F" or k == "L":
-            right = (right - left) / 2 + left
-        else:
-            left = (right - left + 1) / 2 + left
-    assert left == right
-    return left
+    return int(
+        bsp.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2
+    )
 
 
 def main():
     with open(sys.argv[1]) as f:
-        return max(narrow(bp[0:7], 127) * 8 + narrow(bp[7:10], 7) for bp in f)
+        return max(narrow(bp[0:7]) * 8 + narrow(bp[7:10]) for bp in f)
 
 
 if __name__ == "__main__":
--- 2020/05/p2.py
+++ 2020/05/p2.py
@@ -5,20 +5,16 @@
 import sys
 
 
-def narrow(bsp, rb):
-    left, right = 0, rb
-    for k in bsp:
-        if k == "F" or k == "L":
-            right = (right - left) / 2 + left
-        else:
-            left = (right - left + 1) / 2 + left
-    assert left == right
-    return left
+
+def narrow(bsp):
+    return int(
+        bsp.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2
+    )
 
 
 def main():
     with open(sys.argv[1]) as f:
-        all_seats = sorted(narrow(bp[0:7], 127) * 8 + narrow(bp[7:10], 7) for bp in f)
+        all_seats = sorted(narrow(bp[0:7]) * 8 + narrow(bp[7:10]) for bp in f)
         low = all_seats[0]
         for s in all_seats:
             if low != s:  # mind the gap
