dbff9ef311c01073ecf43728ab4117209d5e226a
bildzeitung <bildzeitung@gmail.com>
2020-12-09 11:21:21 -0500

    Day 10

---
+++ 2020/10/p1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+  Day 10
+"""
+import sys
+from collections import defaultdict
+
+
+def main():
+  with open(sys.argv[1]) as f:
+    joltages = [int(x.strip()) for x in f]
+
+  joltages = sorted(joltages)
+  print(joltages)
+  er = 0
+  delta = defaultdict(int)
+  for j in joltages:
+    delta[j-er] += 1
+    er = j
+  delta[3] += 1  # last one
+  print(delta)
+
+  return delta[1] * delta[3]
+
+
+if __name__ == "__main__":
+  print(main())
---
+++ 2020/10/p2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+  Day 10
+"""
+import sys
+from collections import defaultdict
+
+
+def main():
+  with open(sys.argv[1]) as f:
+    joltages = [int(x.strip()) for x in f]
+
+  joltages = sorted(joltages)
+  joltages.insert(0, 0)  # make sure we have the right beginning
+
+  """ Build up a tree of possible bi-(tri-)furcations
+
+      - only care about the parents of a node, so just store those
+  """
+  tree = {}
+  prev = []  # running set of potential parents
+  for i, j in zip(joltages, joltages[1:]):
+    prev = [x for x in prev if j - x < 4]
+    tree[j] = prev
+    prev.append(i)
+
+  """ Roll up the tree, calculating sums as we go
+  """
+  sums = defaultdict(int)
+  for j in reversed(joltages):
+    if j not in tree:  # this is the top of the tree
+      return sums[j]
+    to_send = sums[j] if j in sums else 1
+    for i in tree[j]:
+      sums[i] += to_send
+
+
+
+if __name__ == "__main__":
+  print(main())
