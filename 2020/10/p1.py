79d7d720a73b1e26d9533c360a98b18d27d4bd78
bildzeitung <bildzeitung@gmail.com>
2020-12-11 08:20:44 -0500

    Lint

--- 2020/10/p1.py
+++ 2020/10/p1.py
@@ -7,21 +7,21 @@ from collections import defaultdict
 
 
 def main():
-  with open(sys.argv[1]) as f:
-    joltages = [int(x.strip()) for x in f]
+    with open(sys.argv[1]) as f:
+        joltages = [int(x.strip()) for x in f]
 
-  joltages = sorted(joltages)
-  print(joltages)
-  er = 0
-  delta = defaultdict(int)
-  for j in joltages:
-    delta[j-er] += 1
-    er = j
-  delta[3] += 1  # last one
-  print(delta)
+    joltages = sorted(joltages)
+    print(joltages)
+    er = 0
+    delta = defaultdict(int)
+    for j in joltages:
+        delta[j - er] += 1
+        er = j
+    delta[3] += 1  # last one
+    print(delta)
 
-  return delta[1] * delta[3]
+    return delta[1] * delta[3]
 
 
 if __name__ == "__main__":
-  print(main())
+    print(main())
