9d98e69670517986b09f15664365988bff16f487
bildzeitung <bildzeitung@gmail.com>
2020-12-14 20:58:36 -0500

    Terrible solution

--- 2020/15/p1.py
+++ 2020/15/p1.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
 """
   Day 15
+
+  Used this for part 1 and brute forced part 2, too.
+
 """
 import sys
 
 
 def main():
-  with open(sys.argv[1]) as f:
-    input = [int(x) for x in f.readline().strip().split(",")]
+    with open(sys.argv[1]) as f:
+        input = [int(x) for x in f.readline().strip().split(",")]
 
-  last_spoken = input[-1]
-  last_seen = {x:i+1 for i, x in enumerate(input[:-1])}
-  turn = len(input) + 1
-  while turn < 2021:
-    if last_spoken not in last_seen:
-      spoken = 0
-      last_seen[last_spoken] = turn - 1
-    else:
-      #print(f"Last seen {last_spoken}: {last_seen[last_spoken]}")
-      spoken = turn - 1 - last_seen[last_spoken]
-      last_seen[last_spoken] = turn - 1
-    #print(f"Turn {turn}, spoke: {spoken}")
-    turn += 1
-    last_spoken = spoken
-  
-  return spoken
+    last_spoken = input[-1]
+    last_seen = {x: i + 1 for i, x in enumerate(input[:-1])}
+    turn = len(input)
+    #  while turn < 2020:  # part 1
+    while turn < 30000000:  # part 2
+        if last_spoken not in last_seen:
+            spoken = 0
+            last_seen[last_spoken] = turn
+        else:
+            # print(f"Last seen {last_spoken}: {last_seen[last_spoken]}")
+            spoken = turn - last_seen[last_spoken]
+            last_seen[last_spoken] = turn
+        # print(f"Turn,{turn+1},spoke,{spoken}")
+        turn += 1
+        last_spoken = spoken
+
+    return spoken
 
 
 if __name__ == "__main__":
