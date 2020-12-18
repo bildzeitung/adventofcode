d9b920a0e9b93126d81af507651a5042ff17eca9
bildzeitung <bildzeitung@gmail.com>
2020-12-18 11:40:03 -0500

    Part 1

---
+++ 2020/18/p1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+  Day 18
+"""
+import sys
+from io import BytesIO
+from tokenize import tokenize, NEWLINE, OP, NUMBER, ENDMARKER
+
+def evaluator(f):
+  opstack = []
+  datastack = []
+  for t in tokenize(f.readline):
+    #print(f"Processing {t}  D: {datastack}  O: {opstack}")
+    if t.type == OP:
+      if t.string in ('*', '+'):
+        opstack.append(t.string)
+      if t.string == '(':
+        datastack.append(t.string)
+      if t.string == ')':
+        arg0 = datastack.pop()
+        datastack.pop()
+        if opstack and datastack and datastack[-1] != '(':
+          arg0 = str(eval(f"{arg0} {opstack.pop()} {datastack.pop()}"))
+        datastack.append(arg0)
+    if t.type == NUMBER:
+      if opstack and datastack and datastack[-1] != '(':
+        datastack.append(str(eval(f"{datastack.pop()} {opstack.pop()} {t.string}")))
+      else:
+        datastack.append(t.string)
+
+    if t.type in (NEWLINE, ENDMARKER):
+      return datastack.pop()
+
+
+def main():
+  with open(sys.argv[1], 'rb') as f:
+    s = [int(evaluator(BytesIO(line))) for line in f]
+    print(s)
+    return sum(s)
+
+if __name__ == "__main__":
+    print(main())
---
+++ 2020/18/p2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+  Day 18
+"""
+import sys
+from io import BytesIO
+from tokenize import tokenize, NEWLINE, OP, NUMBER, ENDMARKER
+
+def postfixer(f):
+  opstack = []
+  emitted = []
+  for t in tokenize(f.readline):
+    #print(f"Processing {t}  D: {datastack}  O: {opstack}")
+    if t.type == OP:
+      if t.string in ('*', '+'):
+        while opstack and opstack[-1] != '(':
+          emitted.append(opstack.pop())
+        opstack.append(t.string)
+      if t.string == '(':
+        opstack.append(t.string)
+      if t.string == ')':
+        while True:
+          arg = opstack.pop()
+          if arg == '(':
+            break
+          emitted.append(arg)
+    if t.type == NUMBER:
+      emitted.append(t.string)
+
+    if t.type in (NEWLINE, ENDMARKER):
+      emitted.extend(opstack)
+      return emitted
+
+
+def solve(p):
+  operands = []
+  for i in p:
+    if i in ('*', '+'):
+      arg0, arg1 = operands.pop(), operands.pop()
+      operands.append(str(eval(f"{arg0} {i} {arg1}")))
+    else:
+      operands.append(i)
+  return int(operands.pop())
+
+
+def main():
+  with open(sys.argv[1], 'rb') as f:
+    s = [solve(postfixer(BytesIO(line))) for line in f]
+    print(s)
+    return sum(s)
+
+if __name__ == "__main__":
+    print(main())
