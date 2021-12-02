099fad3d693a7a564f9ea8c6e0793d9b656c9616
bildzeitung <bildzeitung@gmail.com>
2021-12-02 09:50:58 -0500

    Day 2

---
+++ 2021/02/p1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+    Day 2
+"""
+from pathlib import Path
+import sys
+
+DIRECTIONS = {
+    "forward": (1, 0),
+    "down": (0, 1),
+    "up": (0, -1),
+}
+
+def main():
+    horiz = 0
+    depth = 0
+    with Path(sys.argv[1]).open() as f:
+        for line in f:
+            items = line.split()
+            direction, amount = items[0].strip(), int(items[1])
+            horiz = DIRECTIONS[direction][0] * amount + horiz
+            depth = DIRECTIONS[direction][1] * amount + depth
+            assert(depth >= 0)
+    print(f"Final h: {horiz}  d: {depth} ==> {horiz*depth}")
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ 2021/02/p2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+    Day 2
+"""
+from pathlib import Path
+import sys
+
+DIRECTIONS = {
+    "forward": (1, 0),
+    "down": (0, 1),
+    "up": (0, -1),
+}
+
+def main():
+    horiz = 0
+    depth = 0
+    aim = 0
+    with Path(sys.argv[1]).open() as f:
+        for line in f:
+            items = line.split()
+            direction, amount = items[0].strip(), int(items[1])
+            if direction == "down":
+                aim += amount
+            if direction == "up":
+                aim -= amount
+            if direction == "forward":
+                horiz += amount
+                depth += aim * amount
+            assert(depth >= 0)
+    print(f"Final h: {horiz}  d: {depth} ==> {horiz*depth}")
+
+
+if __name__ == "__main__":
+    print(main())
