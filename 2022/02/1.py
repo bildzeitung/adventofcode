c36178e7952cdd2b9d269cd4a2ffd1bfe31173d9
bildzeitung <bildzeitung@gmail.com>
2022-12-02 00:48:05 -0500

    Day 2

---
+++ 2022/02/1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+    Day 2
+"""
+import sys
+from pathlib import Path
+
+opponent = {
+    "A" : "rock",
+    "B" : "paper",
+    "C" : "scissors"
+}
+
+strategy = {
+    "X" : "rock",
+    "Y" : "paper",
+    "Z" : "scissors"
+}
+
+winners = (
+    ("rock", "paper"),
+    ("paper", "scissors"),
+    ("scissors", "rock")
+)
+
+score = {
+    "rock": 1,
+    "paper": 2,
+    "scissors": 3
+}
+
+
+def main():
+    t = 0
+    with Path(sys.argv[1]).open() as f:
+        for line in f:
+            r = line.strip().split(" ")
+            p = (opponent[r[0]], strategy[r[1]])
+
+            t += score[p[1]]  # lose
+
+            if p[0] == p[1]:  # tie
+                t += 3
+
+            if p in winners:  # win
+                t += 6
+
+    print(f"Total score: {t}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/02/2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+    Day 2
+"""
+import sys
+from pathlib import Path
+
+opponent = {
+    "A" : "rock",
+    "B" : "paper",
+    "C" : "scissors"
+}
+
+strategy = {
+    "X" : {  # lose
+            "rock": "scissors",
+            "paper": "rock",
+            "scissors": "paper"
+    },
+    "Y" : {  # draw
+            "rock": "rock",
+            "paper": "paper",
+            "scissors": "scissors"
+    },
+    "Z" : {  # win
+            "rock": "paper",
+            "paper": "scissors",
+            "scissors": "rock"
+    }
+}
+
+score = {
+    "rock": 1,
+    "paper": 2,
+    "scissors": 3
+}
+
+winners = (
+    ("rock", "paper"),
+    ("paper", "scissors"),
+    ("scissors", "rock")
+)
+
+
+def main():
+    t = 0
+    with Path(sys.argv[1]).open() as f:
+        for line in f:
+            r = line.strip().split(" ")
+            p = (opponent[r[0]], strategy[r[1]][opponent[r[0]]])
+
+            t += score[p[1]]  # lose
+
+            if p[0] == p[1]:  # tie
+                t += 3
+
+            if p in winners:  # win
+                t += 6
+
+    print(f"Total score: {t}")
+
+
+if __name__ == "__main__":
+    main()
